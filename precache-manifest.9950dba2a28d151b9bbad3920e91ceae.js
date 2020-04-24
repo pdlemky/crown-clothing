1c3d9c88f5735096b21459e57391de5ae9c76e3f
Paul Lemky <paul@Pauls-MacBook-Pro.local>
2020-04-24 13:28:55 -0700

    Updates

---
+++ precache-manifest.9950dba2a28d151b9bbad3920e91ceae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d6c352b8d7965698c8775da44f9262",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "5f77ad19caba10d46201",
+    "url": "/crown-clothing/static/css/main.3e9c82d2.chunk.css"
+  },
+  {
+    "revision": "c63af9088049b1899f31",
+    "url": "/crown-clothing/static/js/2.aa1e062b.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/crown-clothing/static/js/2.aa1e062b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f77ad19caba10d46201",
+    "url": "/crown-clothing/static/js/main.a4d86555.chunk.js"
+  },
+  {
+    "revision": "ef460ffe6b75c3aad03a",
+    "url": "/crown-clothing/static/js/runtime-main.9a7f6cb7.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
